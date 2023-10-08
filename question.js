522c0ef64e7fd5d31ebc77d1053f2ce93e02e2a1
Vulps <ajmcallister5@gmail.com>
2023-10-08 15:30:34 +0100

    Fix "false" default value in Question

--- question.js
+++ question.js
@@ -1,21 +1,21 @@
 
 class Question {
-  
-  constructor(question,creator){
-    this.id = null;
-    this.question = question
-    this.creator = creator
-    this.isBanned = false
-  }
 
-  toJson(){
-    let string = {}
-    string.id = this.id ?? "No ID";
-    string.question = this.question;
-    string.creator = this.creator;
-    string.isBanned = this.isBanned;
-    return string;
-  }
+	constructor(question, creator) {
+		this.id = null;
+		this.question = question
+		this.creator = creator
+		this.isBanned = 0
+	}
+
+	toJson() {
+		let string = {}
+		string.id = this.id ?? "No ID";
+		string.question = this.question;
+		string.creator = this.creator;
+		string.isBanned = this.isBanned;
+		return string;
+	}
 }
 
 module.exports = Question
