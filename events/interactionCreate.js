bec4f5718f1b608289fe5baaa893aafc10dd64a4
Vulps <ajmcallister5@gmail.com>
2023-10-09 17:14:52 +0100

    Fixed Uncaught exception when new servers run setup

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,44 +5,48 @@ module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
 		if (!interaction.isChatInputCommand()) return;
-
-		const command = interaction.client.commands.get(interaction.commandName);
-
-		if (!command) {
-			console.error(
-				`No command matching ${interaction.commandName} was found.`
-			);
-			return;
-		}
-
-		const key = interaction.guildId
-		const guild = await new UserHandler().findGuild(key)
-		if(guild.isBanned) {
-			interaction.reply('Your Community has been banned for violating the bot\'s Terms of Use');
-			return;
-		}
-
-		if (!interaction.channel.nsfw) {
-			interaction.reply("My commands can only be used on channels marked as NSFW")
-			return;
-		}
-
-		if ((!guild || !guild.hasAccepted) && !(interaction.commandName === "setup" || interaction.commandName === "accept-terms")) {
-			interaction.reply("A community Administrator must first run the /setup command before you can use me");
-			return;
-		}
-
-		if (guild && (guild.name === undefined || guild.name === null)) {
-			guild.name = interaction.guild.name;
-			db.set('guilds', guild).then(() => {
-				console.log(
-					"Server updated with name"
-				)
-			})
-		}
-
-
 		try {
+			const command = interaction.client.commands.get(interaction.commandName);
+
+			if (!command) {
+				console.error(
+					`No command matching ${interaction.commandName} was found.`
+				);
+				return;
+			}
+
+			const key = interaction.guildId
+			const guild = await new UserHandler().findGuild(key)
+
+			if ((!guild || !guild.hasAccepted) && !(interaction.commandName === "setup" || interaction.commandName === "accept-terms")) {
+				interaction.reply("A community Administrator must first run the /setup command before you can use me");
+				return;
+			}
+			if (!(interaction.commandName === "setup" || interaction.commandName === "accept-terms")) { //only run checks for non setup commands
+				if (guild.isBanned) {
+					interaction.reply('Your Community has been banned for violating the bot\'s Terms of Use');
+					return;
+				}
+
+				if (!interaction.channel.nsfw) {
+					interaction.reply("My commands can only be used on channels marked as NSFW")
+					return;
+				}
+
+
+
+				if (guild && (guild.name === undefined || guild.name === null)) {
+					guild.name = interaction.guild.name;
+					db.set('guilds', guild).then(() => {
+						console.log(
+							"Server updated with name"
+						)
+					})
+				}
+			}
+
+
+
 			await command.execute(interaction);
 		} catch (error) {
 			console.error(`Error executing ${interaction.commandName}`);
