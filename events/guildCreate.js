4aca626f6225f655cb7d33fe699755bafa0faffe
Vulps <ajmcallister5@gmail.com>
2023-10-11 18:07:37 +0100

    Added Guild Create Event

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,14 +2,14 @@ const { Events, WebhookClient } = require("discord.js")
 const Database = require("../database")
 
 module.exports = {
-	name: Events.GuildDelete,
+	name: Events.GuildCreate,
 	async execute(guild) {
 		const db = new Database();
 		console.log(guild);
 		db.delete('guilds', guild.id).then( () => {
 			const webhookClient = new WebhookClient({ url: process.env.WEBHOOK_SERVER_URL});
  
-			webhookClient.send(`Bot has been removed from server: ${guild.name}\n\n || ID: ${guild.id} ||`);
+			webhookClient.send(`Bot has been added to server: ${guild.name}\n\n || ID: ${guild.id} ||`);
 		})
 
 	}
