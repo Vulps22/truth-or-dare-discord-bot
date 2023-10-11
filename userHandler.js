a472edba7d1f9257481387c16b3a25915bfcd250
Vulps <ajmcallister5@gmail.com>
2023-10-12 00:07:14 +0100

    Spelling Mistake in new function

--- userHandler.js
+++ userHandler.js
@@ -78,7 +78,7 @@ class UserHandler extends Handler {
 		const statusChannel = client.channels.cache.get(process.env.STATUS_CHANNEL_ID);
 
 		// Update channel name with guild count
-		statusChannel.setName(`Servers: ${client.guilds.cache.size}`);
+		statusChannel.setName(`Connected Servers: ${client.guilds.cache.size}`);
 
 		console.log(`Connected Servers: ${client.guilds.cache.size}`);
 	}
