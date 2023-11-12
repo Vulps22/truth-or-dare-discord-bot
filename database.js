c0e8771b03e0b8c67745e1b3128eca6cfac720f6
Vulps <ajmcallister5@gmail.com>
2023-11-12 21:23:44 +0000

    removed useless Database constructor

--- database.js
+++ database.js
@@ -4,19 +4,6 @@ class Database {
 
 	connection;
 
-	constructor() {
-		this.connection = mysql.createConnection({
-			host: process.env['DATABASE_HOST'],
-			port: process.env['DATABASE_DOCKER_PORT'],
-			user: process.env['DATABASE_USER'],
-			password: process.env['DATABASE_PASSWORD'],
-			database: process.env['DATABASE']
-		});
-		this.connection.connect(function (err) {
-			if (err) throw err;
-		});
-	}
-
 	connect() {
 		this.connection = mysql.createConnection({
 			host: process.env['DATABASE_HOST'],
