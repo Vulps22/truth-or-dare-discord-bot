7fa642137fbda2827bb5c54f79c365d7143ba9f3
Vulps <ajmcallister5@gmail.com>
2024-11-25 00:40:56 +0000

    Fix Broken Tests Found in Vulps22/truth-or-dare-discord-bot#1 Vulps22/truth-or-dare-discord-bot#2 Vulps22/truth-or-dare-discord-bot#3

--- commands/global/leaderboard.js
+++ commands/global/leaderboard.js
@@ -38,7 +38,7 @@ module.exports = {
 				}
 				const premium = await server.hasPremium();
 				if (!premium) {
-					interaction.reply({ content: "This is a premium command. Premium is not quite ready yet, But I'm working hard to make these commands available for everyone :)", ephemeral: true });
+					interaction.editReply({ content: "This is a premium command. Premium is not quite ready yet, But I'm working hard to make these commands available for everyone :)", ephemeral: true });
 
 					//interaction.sendPremiumRequired();
 					return;
