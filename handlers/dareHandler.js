5b421f5bc2a97f8aa667a5fb347ca1f7de875519
Vulps <ajmcallister5@gmail.com>
2025-03-31 20:43:54 +0100

    #59 - Remove unused functions from TruthHandler and DareHandler

--- handlers/dareHandler.js
+++ handlers/dareHandler.js
@@ -1,13 +1,5 @@
-// eslint-disable-next-line no-unused-vars
-const { Interaction, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Guild, MessageFlags } = require('discord.js');
-
 const Handler = require('handlers/handler.js')
-// eslint-disable-next-line no-unused-vars
-const User = require('objects/user.js');
-const Dare = require('objects/dare.js');
-const logger = require('objects/logger.js');
-// eslint-disable-next-line no-unused-vars
-let client = null
+
 class DareHandler extends Handler {
 
 	successXp = 50;
@@ -17,69 +9,6 @@ class DareHandler extends Handler {
 		super("dare")
 		this.client = client
 	}
-
-	/**
-	 * 
-	 * @param {Interaction} interaction 
-	 * @returns 
-	 * @deprecated Use parent function instead
-	 */
-	async vote(interaction) {
-		super.vote(interaction);
-
-	}
-	/**
-	 * 
-	 * @param {Interaction} interaction 
-	 * @param {UserDare} userDare 
-	 * @param {string} dareUser 
-	 * @param {User} user 
-	 * @returns 
-	 * @deprecated Use parent function instead
-	 */
-	async doSkip(interaction, userDare, dareUser, user) {
-		super.doSkip(interaction, userDare, dareUser, user);
-	}
-
-	/**
-	 * 
-	 * @param {Interaction} interaction 
-	 * @param {UserDare} userDare 
-	 * @param {String} dareUser 
-	 * @param {User} user 
-	 * @param {Server} server 
-	 * @returns 
-	 * @deprecated Use parent function instead
-	 */
-	async doVote(interaction, userDare, dareUser, user, server) {
-
-		super.doVote(interaction, userDare, dareUser, user, server);
-	}
-
-	/**
-	 * mark the dare as approved or banned
-	 * @param {Interaction} interaction 
-	 * @param {string<"ban"|"approve"} decision 
-	 */
-	async setDare(interaction, decision) {
-		if (!interaction.deferred) await interaction.deferReply({ ephemeral: true })
-		let dare = await new Dare().find(interaction.message.id);
-		switch (decision) {
-			case "ban":
-				this.getBanReason(interaction, dare.id);
-				break;
-			case 'unban':
-				await dare.unBan();
-				logger.updateDare(dare);
-				interaction.editReply("Truth has been Unbanned");
-				break;
-			case "approve":
-				this.approve(interaction, dare);
-				break;
-		}
-
-	}
-
 }
 
 module.exports = DareHandler;
--- handlers/truthHandler.js
+++ handlers/truthHandler.js
@@ -1,24 +1,4 @@
-const { 
-	/* eslint-disable no-unused-vars */
-	Interaction, 
-	EmbedBuilder, 
-	ActionRowBuilder, 
-	ButtonBuilder, 
-	ButtonStyle, 
-	Client, 
-	MessageFlags
-} = require('discord.js');
-
 const Handler = require('handlers/handler.js')
-const UserTruth = require('objects/userTruth.js');
-const User = require('objects/user.js');
-const Server = require('objects/server.js');
-const Truth = require('objects/truth.js');
-const logger = require('objects/logger.js');
-const Question = require('objects/question.js');
-const GivenQuestion = require('objects/givenQuestion.js');
-const Purchasable = require('objects/purchasable');
-let client = null;
 
 class TruthHandler extends Handler {
 
@@ -28,30 +8,6 @@ class TruthHandler extends Handler {
 	constructor(client) {
 		super("truth")
 		this.client = client
-	}
-
-	/**
- * mark the truth as approved or banned
- * @param {Interaction} interaction 
- * @param {string<"ban"|"approve">} decision 
- */
-	async setTruth(interaction, decision) {
-		if (!interaction.deferred) await interaction.deferReply({ ephemeral: true });
-		let truth = await new Truth().find(interaction.message.id);
-		switch (decision) {
-			case "ban":
-				this.getBanReason(interaction, truth.id);
-				break;
-			case 'unban':
-				await truth.unBan();
-				logger.updateTruth(truth);
-				interaction.editReply("Truth has been Unbanned");
-				break;
-			case "approve":
-				this.approve(interaction, truth);
-				break;
-		}
-
 	}
 }
 
