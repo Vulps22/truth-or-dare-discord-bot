0de55502d83bdffe0d9146c14ec09a6533138a2c
Vulps <ajmcallister5@gmail.com>
2025-07-26 17:44:21 +0100

    #91 - Delete button report_action-ban (now a select menu)

--- buttons/report_action_ban.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'report_action-ban',
-  async execute(interaction) {
-    // TODO: Implement logic to ban based on selected reason
-    await interaction.reply({ content: 'Ban action executed.', ephemeral: true });
-  }
-};
